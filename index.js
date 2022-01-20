207efbc859eaa25c5cb682ab4cce196a7c72f3c6
Eric <ericontiveros123@gmail.com>
2022-01-20 14:23:38 -0700

    commit tree f8ba30c4d5173de5f3c3fdc2bd82cff956f02239

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(array, cb){
+    const newArray = array.map(element => {
+  return cb(element);
+});
+return newArray;
+}
+
+function reduce(array, cb, startPoint = 0) {
+    let t = (!!startPoint) ? startPoint : array[0]
+    let i = (!!startPoint) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        t = cb(array[i], t)
+    }
+    return t
+}
+
